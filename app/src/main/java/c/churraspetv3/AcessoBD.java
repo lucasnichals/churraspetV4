b7cbfbdeec5a3669319a8415532ca8228a45b5cf
lucasnichals <lucas.nichals.ifpinhais@gmail.com>
2023-06-19 23:50:06 -0300

    Add table products

--- app/src/main/java/c/churraspetv3/AcessoBD.java
+++ app/src/main/java/c/churraspetv3/AcessoBD.java
@@ -12,21 +12,35 @@ public class AcessoBD extends SQLiteOpenHelper {
     protected static final String TABELA_USUARIO = "TABELA_USUARIO";
     protected static final String USUARIO_ID = "ID";
     protected static final String USUARIO_NOME = "USUARIO_NOME";
-    protected static final String USUARIO_SENHA = "USUARIO_IDADE";
+    protected static final String USUARIO_SENHA = "USUARIO_SENHA";
+
+    protected static final String TABELA_PRODUTOS = "TABELA_PRODUTOS";
+    protected static final String PRODUTO_ID = "ID";
+    protected static final String PRODUTO_NOME = "PRODUTO_NOME";
+    protected static final String PRODUTO_DESCRICAO = "PRODUTO_DESCRICAO";
+    protected static final String PRODUTO_QUANTIDADE = "PRODUTO_QUANTIDADE";
+    protected static final String PRODUTO_PRECO = "PRODUTO_PRECO";
+
 
 
     public AcessoBD(@Nullable Context context) {
-        super(context, "ClienteBD", null, 1);
+        super(context, "ChurrasPetBD", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String statement = "CREATE TABLE " + TABELA_USUARIO +
+        String statementUsuario = "CREATE TABLE " + TABELA_USUARIO +
                 " (" + USUARIO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + USUARIO_NOME + " TEXT, " + USUARIO_SENHA + " TEXT)";
-        db.execSQL(statement);
-    }
+        db.execSQL(statementUsuario);
 
+        String statementProdutos = "CREATE TABLE " + TABELA_PRODUTOS +
+                " (" + PRODUTO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PRODUTO_NOME + " TEXT, " + PRODUTO_DESCRICAO + " TEXT, "
+                + PRODUTO_QUANTIDADE + " INT, "
+                + PRODUTO_PRECO + " FLOAT)";
+        db.execSQL(statementProdutos);
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
